b81b3c509af7a7f27b321486e1aec271acd0511e
Moshe Sommers <msommers@mainsequence.net>
2020-02-05 14:35:15 -0500

    Updates

---
+++ precache-manifest.4d319d82b0b3b7a6a39b1e8c79a1f59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d53ab02ca1d9ed8b72ce08f98abecc",
+    "url": "/lamdaini/index.html"
+  },
+  {
+    "revision": "87906171800497de9444",
+    "url": "/lamdaini/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "49f99f3f3867b6caaa56",
+    "url": "/lamdaini/static/js/2.9793703b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lamdaini/static/js/2.9793703b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87906171800497de9444",
+    "url": "/lamdaini/static/js/main.4e40b3e9.chunk.js"
+  },
+  {
+    "revision": "8701ba6e8d7c1844836b",
+    "url": "/lamdaini/static/js/runtime-main.9276a73a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lamdaini/static/media/logo.5d5d9eef.svg"
+  }
+]);
