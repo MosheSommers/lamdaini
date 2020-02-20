eab480c111202ebb5c4d01c2118c405f798984ff
Moshe Sommers <msommers@mainsequence.net>
2020-02-20 11:49:14 -0500

    Updates

---
+++ precache-manifest.97b1be4586f9ddcd6b12aed54a3e0b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c529d9e1a7fc11d4522ae00418d55a",
+    "url": "/lamdaini/index.html"
+  },
+  {
+    "revision": "2fa3594978419e9a8093",
+    "url": "/lamdaini/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c17b96265702e784aa00",
+    "url": "/lamdaini/static/js/2.8b378a6c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lamdaini/static/js/2.8b378a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa3594978419e9a8093",
+    "url": "/lamdaini/static/js/main.96ddc26d.chunk.js"
+  },
+  {
+    "revision": "8701ba6e8d7c1844836b",
+    "url": "/lamdaini/static/js/runtime-main.9276a73a.js"
+  },
+  {
+    "revision": "70ee0874375376da693c9f2a6a5ef0f5",
+    "url": "/lamdaini/static/media/background.70ee0874.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed70884059c5439c5b732c29b74f2e2e.js"
+  "/lamdaini/precache-manifest.97b1be4586f9ddcd6b12aed54a3e0b56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lamdaini/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
